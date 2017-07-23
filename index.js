dc5265fee4b1cb98a9ad65500bacaa59681c5071
Maiko Kuppe <maiko@kuppe.eu>
2017-07-23 17:12:42 +0200

    filter compatible tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const args = require('args')
-const test = require('./lib/test')
+const main = require('./lib/main')
 
 args
   .option('domain', 'The API host domain to use', 'www.james24apis.com')
@@ -11,4 +11,4 @@ args
 
 const flags = args.parse(process.argv)
 
-test(flags)
+main(flags)
