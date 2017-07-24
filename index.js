d448b94c224ef1a75e4bd32bf8d964b8fa3cd6c4
Maiko Kuppe <maiko@kuppe.eu>
2017-07-24 09:30:57 +0200

    run test callbacks with domain & use default include parameter

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const main = require('./lib/main')
 args
   .option('domain', 'The API host domain to use', 'www.james24apis.com')
   .option('ecosystem', 'Name of the ecosystem to test. The necessary tests are derived from this.', 'james')
-  .option('include', 'Microservice versions to (additionally) include in tests.')
+  .option('include', 'Microservice versions to (additionally) include in tests.', '{}')
   .example('api-test -d dev.james24apis.com -e james', 'Performs all tests cases required by the production `james` environment, using the base url `https://dev.james24apis.com`.')
   .example(`api-test -d users.staging.james24apis.com -e james -i '{"users":["v1","v2"]}'`, 'Performs all tests cases required by the production `james` environment - including those, which can be performed using the versions `v1` and `v2` of the `users` microservice - using the base url `https://users.staging.james24apis.com`.')
 
